b386c4029cd6baed0d850be20e2ee39e91b75361
Joe Hebert <raphaelhebert18@gmail.com>
2021-07-13 12:03:23 +0700

    added route and menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,50 @@
 import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import styled from 'styled-components'
 
-const Navbar = (props) => {
-  const toggleMode = e => {
-    e.preventDefault();
-    props.setLocalDark(!props.localDark);
-  };
+
+const MenuButton = styled.div`
+  display: flex;
+  position: relative;
+  flex-flow: column nowrap;
+  &:hover div{
+    display: block;
+  }
+`
+
+const Menu = styled.div`
+    position: absolute;
+    display: none;
+`
+const MenuName = styled.h2`
+  position: absolute ;
+`
+
+const Button = styled.button`
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+
+const Navbar = ({localDark, setLocalDark}) => {
+
+  const history = useHistory();
 
   return (
     <nav className="navbar">
+      <MenuButton>
+        <MenuName>Menu</MenuName>
+        <Menu>
+          <Button type="button" onClick={() => {history.push('/Home')}} >Home</Button> 
+          <Button type="button" onClick={() => {history.push('/isItDark')}} >Is it Dark?</Button>
+        </Menu>
+      </MenuButton>
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
-          className={props.darkMode ? 'toggle toggled' : 'toggle'}
+          onClick={() => setLocalDark(!localDark)}
+          className={localDark ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
